b21ac757c2230985db370bc348762237b9570df5
Jonel Briones <jonel.c.briones@gmail.com>
2022-05-19 03:44:42 -0700

    fixed node problem

--- SinglyLinkedLists/Fronts/index.js
+++ SinglyLinkedLists/Fronts/index.js
@@ -1,17 +1,17 @@
-class Node {
+class SLLNode {
     constructor(data) {
-        this.data = data;
+        this.value = data;
         this.next = null;
     }
 }
 
 
-class LinkedList {
+class SLL {
     constructor() {
         this.head = null;
     }
-    addFront(val) {
-        let new_node = new Node( 10 );
+    addFront(value) {
+        let new_node = new SLLNode(value);
         if(!this.head) {
             this.head = new_node;
             return this;
@@ -20,7 +20,32 @@ class LinkedList {
         this.head = new_node;
         return this;
     }
+    // we make the node we want to remove point to a null and have it's previous next become the head.
+    removeFront() {
+        if(this.head === null) {
+            return this.head;
+        }
+        let removeNode = this.head;
+        this.head = removeNode.next;
+        removeNode.next = null;
+        return this;
+    }
+    front() {
+        if(this.head === null) {
+            return this;
+        } else return this.head.value;
+    }
 }
+let new_node = new SLL();
+new_node.addFront(10);
+new_node.addFront(1);
+new_node.addFront(5);
 
-new_node = new Node(10)
-another_node = new Node(8)
+ // will remove the the node that is pointed to a null/end of the node list
+new_node.removeFront();
+
+// displays the last
+console.log(new_node.front());
+
+// console.log(new_node)
+// console.log(new_node.next)
